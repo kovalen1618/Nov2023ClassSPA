fda75139e5dd1cdc8938e6a3f710bf78b4b878e7
kovalen1618 <kovalen1.618@gmail.com>
2023-12-13 14:20:42 -0600

    Creates state-driven SPA

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-import { Header, Nav, Main, Footer } from "./components";
+// Libraries
+import Navigo from "navigo";
+import { capitalize } from "lodash";
 
-function render() {
+// Components & Views
+import { Header, Nav, Main, Footer } from "./components";
+import * as store from "./store";
+
+const router = new Navigo("/");
+router
+  .on({
+    "/": () => render(),
+    ":view": params => {
+      let view = capitalize(params.data.view);
+      if (view in store) {
+        render(store[view]);
+      } else {
+        render(store.Viewnotfound);
+        console.log(`View ${view} not defined`);
+      }
+    }
+  })
+  .resolve();
+
+function render(state = store.Home) {
   document.querySelector("#root").innerHTML = `
-      ${Header()}
-      ${Nav()}
-      ${Main()}
+      ${Header(state)}
+      ${Nav(store.Links)}
+      ${Main(state)}
       ${Footer()}
   `;
+  router.updatePageLinks();
 }
 
 render();
 
 // add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+// document.querySelector(".fa-bars").addEventListener("click", () => {
+//   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+// });
