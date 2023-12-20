9d23d791bc22022a568308bc928a7bb831d2a156
kovalen1618 <kovalen1.618@gmail.com>
2023-12-19 21:25:29 -0600

    Calls to pizza and weather APIs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Libraries
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import axios from "axios";
 
 // Components & Views
 import { Header, Nav, Main, Footer } from "./components";
@@ -27,6 +28,66 @@ function afterRender() {
   });
 }
 
+router.hooks({
+  before: (done, params) => {
+    // We need to know what view we are on to know what data to fetch
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
+    // Add a switch case statement to handle multiple routes
+    switch (view) {
+      case "Home":
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
+          )
+          .then(response => {
+            const kelvinToFahrenheit = kelvinTemp =>
+              Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+            store.Home.weather = {
+              city: response.data.name,
+              temp: kelvinToFahrenheit(response.data.main.temp),
+              feelsLike: kelvinToFahrenheit(response.data.main.feels_like),
+              description: response.data.weather[0].main
+            };
+            done();
+          })
+          .catch(err => {
+            console.log(err);
+            done();
+          });
+        break;
+      // Add a case for each view that needs data from an API
+      case "Pizza":
+        // New Axios get request utilizing already made environment variable
+        axios
+          .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
+          .then(response => {
+            // We need to store the response to the state, in the next step but in the meantime let's see what it looks like so that we know what to store from the response.
+            console.log("response", response);
+            store.Pizza.pizzas = response.data;
+            done();
+          })
+          .catch(error => {
+            console.log("It puked", error);
+            done();
+          });
+        break;
+      default:
+        done();
+    }
+  },
+  already: params => {
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
+
+    render(store[view]);
+  }
+});
+
 router
   .on({
     "/": () => render(),
@@ -41,3 +102,7 @@ router
     }
   })
   .resolve();
+
+// axios.get(
+//   `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
+// );
